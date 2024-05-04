117ef439bc0db400803ac7616ffd82d7fdb26d42
John Omotani <john.omotani@ukaea.uk>
2024-05-04 18:02:08 +0100

    Split plotting of state variables and flow/Epar

--- animate-results.py
+++ animate-results.py
@@ -4,5 +4,8 @@ from xbout import open_boutdataset
 
 ds = open_boutdataset("data/BOUT.dmp.*.nc", info=False).squeeze()
 
-#ds.bout.animate_list([["n", "V_i", "pperp_i", "par_i", "pperp_e", "ppar_e"]], vmin=-0.2, vmax=0.4, show=True)
-ds.bout.animate_list([["n", "V_i", "pperp_i", "ppar_i", "pperp_e", "ppar_e"]], show=True)
+#ds.bout.animate_list([["n", "V_i", "pperp_i", "ppar_i", "pperp_e", "ppar_e", "Epar"]], vmin=-0.1,
+#                     vmax=1.4, show=True)
+ds.bout.animate_list([["n", "pperp_i", "ppar_i", "pperp_e", "ppar_e"]], show=True)
+
+ds.bout.animate_list([["V_i", "Epar"]], show=True)
